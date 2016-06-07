1edd1f589c35f8917657c1481d459b523ae062e2
Matthias Richard <matthias.rms@gmail.com>
2016-06-07 17:33:41 +0200

    fix

--- app/bot/answer/navigation.rb
+++ app/bot/answer/navigation.rb
@@ -11,35 +11,33 @@ def navigation(session, username, sender, msg = "")
     transfer_middle_office(session.id, sender, "hey")
     sleep(3)
     cta_intent_message(session, sender)
-    context["intent"] == "start"
+  elsif context["intent"] == "start"
+    sender.reply({text: t('navigation-start-validation')})
+    transfer_middle_office(session.id, sender, t('navigation-start-validation'))
+    cta_intent_message(session, sender)
+  elsif context["intent"] == "stop"
+    sender.rep#ly({text: t('stop')})
+    transfer_middle_office(session.id, sender, t('stop'))
+  elsif context["intent"] == "help"
+    session.update(status: "human")
+    sender.reply({text: t('human-assistance-requested')})
+    transfer_middle_office(session.id, sender, t('human-assistance-requested'))
+   help_request(username)
+  elsif context["intent"] == "exit"
+    context = {}
     session.update(context: context)
-  # elsif context["intent"] == "start"
-  #   sender.reply({text: t('navigation-start-validation')})
-  #   transfer_middle_office(session.id, sender, t('navigation-start-validation'))
-  #   cta_intent_message(session, sender)
-  # elsif context["intent"] == "stop"
-  #   sender.rep#ly({text: t('stop')})
-  #   transfer_middle_office(session.id, sender, t('stop'))
-  # elsif context["intent"] == "help"
-  #   session.update(status: "human")
-  #   sender.reply({text: t('human-assistance-requested')})
-  #   transfer_middle_office(session.id, sender, t('human-assistance-requested'))
-  #  help_request(username)
-  # elsif context["intent"] == "exit"
-  #   context = {}
-  #   session.update(context: context)
-  #   sender.reply({text: t('exit-requested')})
-  #   transfer_middle_office(session.id, sender, t('exit-requested'))
-  # elsif context["intent"] == "restart"
-  #   sender.reply({text: t('restart-requested')})
-  #   transfer_middle_office(session.id, sender, t('restart-requested'))
-  #   cta_restart_message(sender)
-  # elsif session.context["intent"] == "lost"
-  #   sender.reply({text: t('lost')})
-  #   transfer_middle_office(session.id, sender, t('lost'))
-  # elsif context["intent"] == "mainbrowsing"
-  #   context = {}
-  #   cta_intent_message(session, sender)
+    sender.reply({text: t('exit-requested')})
+    transfer_middle_office(session.id, sender, t('exit-requested'))
+  elsif context["intent"] == "restart"
+    sender.reply({text: t('restart-requested')})
+    transfer_middle_office(session.id, sender, t('restart-requested'))
+    cta_restart_message(sender)
+  elsif session.context["intent"] == "lost"
+    sender.reply({text: t('lost')})
+    transfer_middle_office(session.id, sender, t('lost'))
+  elsif context["intent"] == "mainbrowsing"
+    context = {}
+    cta_intent_message(session, sender)
   end
 end
 
--- app/bot/answer/transfer_middle_office.rb
+++ app/bot/answer/transfer_middle_office.rb
@@ -1,8 +1,8 @@
 def transfer_middle_office(session_id, sender, msg = "")
   # SETUP API KEYS
   # TODO: add lines for api
-  session = Session.find(session_id)
-  sender_id = session.facebook_id
+  # session = Session.find(session_id)
+  # sender_id = session.facebook_id
   #username = sender.get_profile[:body]["first_name"]
   #profile_picture = {profile_pic: (sender.get_profile[:body]["profile_pic"])}.to_query
   #url = "https://mymessagingstore.herokuapp.com/api/v1/sessions?fbid=#{sender_id}&msg=#{msg}&first_name=#{username}&sender=bot&context=#{session.context}&#{profile_picture}"
