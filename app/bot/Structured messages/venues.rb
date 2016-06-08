2d6f34c122a0b7b72a543f400efff77b681c477a
Matthias Richard <matthias.rms@gmail.com>
2016-06-08 14:35:28 +0200

    fixed method call

--- app/bot/Structured messages/venues.rb
+++ app/bot/Structured messages/venues.rb
@@ -1,6 +1,7 @@
 # Method of allowing the user to share his/her location and get a carousel of the nearest venues
 def choose_location(session, sender)
   p "CHOOSE LOCATION"
+  context = session.context
   geo_loc = "http://api.songkick.com/api/3.0/search/locations.json?location=geo:#{context['lat']},#{context['lng']}&apikey=h76Z5PDgOid28Zly"
   p geo_loc
   locations =  Oj.load(RestClient.get geo_loc, :content_type => :json, :accept => :json)["resultsPage"]["results"]["location"][0..2]
