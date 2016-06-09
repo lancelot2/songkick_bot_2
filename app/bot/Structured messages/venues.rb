7d633c1a798eff69d13a1e83198788267ad42520
Matthias Richard <matthias.rms@gmail.com>
2016-06-09 19:48:59 +0200

    fix

--- app/bot/Structured messages/venues.rb
+++ app/bot/Structured messages/venues.rb
@@ -19,6 +19,7 @@ end
 
 
 def show_venues(session, sender)
+  context = session.context
   structured_reply = GenericTemplate.new
   venue_url = "http://api.songkick.com/api/3.0/search/venues.json?query=#{context['city']}&apikey=h76Z5PDgOid28Zly"
   response =  Oj.load(RestClient.get venue_url, :content_type => :json, :accept => :json)["resultsPage"]["results"]["venue"][9]
