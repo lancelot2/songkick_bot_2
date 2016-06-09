ef87d64123f7da2c4f66ee5b91b1534b5bdeee35
Matthias Richard <matthias.rms@gmail.com>
2016-06-09 22:16:04 +0200

    added venue show concerts

--- app/bot/Structured messages/venues.rb
+++ app/bot/Structured messages/venues.rb
@@ -24,7 +24,7 @@ def show_venues(session, sender, lat = "0", lng = "0" )
   if lat == "0"
     venue_url = "http://api.songkick.com/api/3.0/search/venues.json?query=#{context['city']}&apikey=h76Z5PDgOid28Zly"
   else
-    venue_url = "http://api.songkick.com/api/3.0/search/venues.json?location=geo:#{context['lat']},#{context['lng']}&apikey={h76Z5PDgOid28Zly}"
+    venue_url = "http://api.songkick.com/api/3.0/search/venues.json?location=geo:#{context['lat']},#{context['lng']}&apikey=h76Z5PDgOid28Zly"
   end
   response =  Oj.load(RestClient.get venue_url, :content_type => :json, :accept => :json)["resultsPage"]["results"]["venue"][0..9]
   response.each do |venue|
