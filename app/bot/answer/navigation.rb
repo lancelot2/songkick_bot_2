f97678ac3abd9cd5d969039f9d7fca027f3873e7
Matthias Richard <matthias.rms@gmail.com>
2016-06-09 22:41:20 +0200

    added venue show concerts

--- app/bot/answer/navigation.rb
+++ app/bot/answer/navigation.rb
@@ -40,6 +40,7 @@ def navigation(session, username, sender, msg = "")
   #   context = {}
   #   cta_intent_message(session, sender)
 elsif context["intent"] == "geolocated"
+    geo_loc = "http://api.songkick.com/api/3.0/search/locations.json?location=geo:#{context['lat']},#{context['lng']}&apikey=h76Z5PDgOid28Zly"
     context["city"] =  Oj.load(RestClient.get geo_loc, :content_type => :json, :accept => :json)["resultsPage"]["results"]["location"][0]["metroArea"]["displayName"]
     venues_or_upcoming(session, sender)
   elsif context["intent"] == "venue"
