e6fbd88cd90053c4fe426b91c8bb28b2ba4b7eb5
Matthias Richard <matthias.rms@gmail.com>
2016-06-10 17:29:31 +0200

    fix

--- app/bot/answer/navigation.rb
+++ app/bot/answer/navigation.rb
@@ -58,6 +58,7 @@ def navigation(session, username, sender, msg = "")
   elsif context["intent"] == "upcoming" && context["artist_id"].present?
     artist_url = "http://api.songkick.com/api/3.0/artists/#{context['artist_id']}/calendar.json?apikey=h76Z5PDgOid28Zly"
     concerts =  Oj.load(RestClient.get artist_url, :content_type => :json, :accept => :json)["resultsPage"]["results"]["event"]
+    increase_context(session)
     generic_template_message(session, concerts, sender, context, msg)
   elsif context["intent"] == "upcoming"
     city(session, sender, msg)
