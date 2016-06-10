69ff633af6cd83820dda1018b4ac1a194f83c2a9
Matthias Richard <matthias.rms@gmail.com>
2016-06-10 16:48:19 +0200

    fix

--- app/bot/Structured messages/Cities/usa.rb
+++ app/bot/Structured messages/Cities/usa.rb
@@ -6,7 +6,7 @@ def usa(sender)
   upcoming_concerts_in_san_francisco_button = Button.new
   upcoming_concerts_in_san_francisco_button.add_postback("Concerts in SF","sf")
   check_venues_in_san_francisco_button = Button.new
-  check_venues_in_san_francisco_button.add_postback("Venues in SF","sf")
+  check_venues_in_san_francisco_button.add_postback("Venues in SF","sf venues")
   structured_reply.add_element("San Francisco", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1465479573/SongKick/cities/san_francisco_3.png", "", [upcoming_concerts_in_san_francisco_button.get_message, check_venues_in_san_francisco_button.get_message] )
 
   upcoming_concerts_in_los_angeles_button = Button.new
@@ -18,49 +18,49 @@ def usa(sender)
   upcoming_concerts_in_new_york_button = Button.new
   upcoming_concerts_in_new_york_button.add_postback("Concerts in NYC","New York")
   check_venues_in_new_york_button = Button.new
-  check_venues_in_new_york_button.add_postback("Venues in NYC","New York")
+  check_venues_in_new_york_button.add_postback("Venues in NYC","New York venues")
   structured_reply.add_element("New York", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712879/SongKick/cities/new_york.png", "", [upcoming_concerts_in_new_york_button.get_message, check_venues_in_new_york_button.get_message] )
 
   upcoming_concerts_in_portland_button = Button.new
   upcoming_concerts_in_portland_button.add_postback("Concerts in Portland","Portland")
   check_venues_in_portland_button = Button.new
-  check_venues_in_portland_button.add_postback("Venues in Portland","Portland")
+  check_venues_in_portland_button.add_postback("Venues in Portland","Portland venues")
   structured_reply.add_element("Portland", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712886/SongKick/cities/portland.png", "", [upcoming_concerts_in_portland_button.get_message, check_venues_in_portland_button.get_message] )
 
   upcoming_concerts_in_washington_button = Button.new
   upcoming_concerts_in_washington_button.add_postback("Concerts in Washington","Washington")
   check_venues_in_washington_button = Button.new
-  check_venues_in_washington_button.add_postback("Venues in Washington","Washington")
+  check_venues_in_washington_button.add_postback("Venues in Washington","Washington venues")
   structured_reply.add_element("Washington", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712864/SongKick/cities/washington.png", "", [upcoming_concerts_in_washington_button.get_message, check_venues_in_washington_button.get_message] )
 
   upcoming_concerts_in_philadelphia_button = Button.new
   upcoming_concerts_in_philadelphia_button.add_postback("Concerts in Philly","Philadelphia")
   check_venues_in_philadelphia_button = Button.new
-  check_venues_in_philadelphia_button.add_postback("Venues in Philly","Philadelphia")
+  check_venues_in_philadelphia_button.add_postback("Venues in Philly","Philadelphia venues")
   structured_reply.add_element("Philadelphia", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712884/SongKick/cities/philadelphia.png", "", [upcoming_concerts_in_philadelphia_button.get_message, check_venues_in_philadelphia_button.get_message] )
 
   upcoming_concerts_in_seattle_button = Button.new
   upcoming_concerts_in_seattle_button.add_postback("Concerts in Seattle","Seattle")
   check_venues_in_seattle_button = Button.new
-  check_venues_in_seattle_button.add_postback("Venues in Seattle","Seattle")
+  check_venues_in_seattle_button.add_postback("Venues in Seattle","Seattle venues")
   structured_reply.add_element("Seattle", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712892/SongKick/cities/seattle.png", "", [upcoming_concerts_in_seattle_button.get_message, check_venues_in_seattle_button.get_message] )
 
   upcoming_concerts_in_chicago_button = Button.new
   upcoming_concerts_in_chicago_button.add_postback("Concerts in Chicago","Chicago")
   check_venues_in_chicago_button = Button.new
-  check_venues_in_chicago_button.add_postback("Venues in Chicago","Chicago")
+  check_venues_in_chicago_button.add_postback("Venues in Chicago","Chicago venues")
   structured_reply.add_element("Chicago", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712862/SongKick/cities/chicago.png", "", [upcoming_concerts_in_chicago_button.get_message, check_venues_in_chicago_button.get_message] )
 
   upcoming_concerts_in_orlando_button = Button.new
   upcoming_concerts_in_orlando_button.add_postback("Concerts in Orlando","Orlando")
   check_venues_in_orlando_button = Button.new
-  check_venues_in_orlando_button.add_postback("Venues in Orlando","Orlando")
+  check_venues_in_orlando_button.add_postback("Venues in Orlando","Orlando venues")
   structured_reply.add_element("Orlando", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712882/SongKick/cities/orlando.png", "", [upcoming_concerts_in_orlando_button.get_message, check_venues_in_orlando_button.get_message] )
 
   upcoming_concerts_in_pittsburg_button = Button.new
   upcoming_concerts_in_pittsburg_button.add_postback("Concerts in Pittsburg","Pittsburg")
   check_venues_in_pittsburg_button = Button.new
-  check_venues_in_pittsburg_button.add_postback("Venues in Pittsburg","Pittsburg")
+  check_venues_in_pittsburg_button.add_postback("Venues in Pittsburg","Pittsburg venues")
   structured_reply.add_element("Pittsburg", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712890/SongKick/cities/pittsburg.png", "", [upcoming_concerts_in_pittsburg_button.get_message, check_venues_in_pittsburg_button.get_message] )
 
   reply_transfer(session, sender, structured_reply)
