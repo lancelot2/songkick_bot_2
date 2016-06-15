da13a1c6184831ef884344f0509056b20c47bcf4
Matthias Richard <matthias.rms@gmail.com>
2016-06-15 09:57:03 +0100

    added mms key

--- app/bot/answer/transfer_middle_office.rb
+++ app/bot/answer/transfer_middle_office.rb
@@ -4,7 +4,7 @@ def transfer_middle_office(session_id, sender, msg = "")
   sender_id = session.facebook_id
   username = sender.get_profile[:body]["first_name"]
   profile_picture = {profile_pic: (sender.get_profile[:body]["profile_pic"])}.to_query
-  url = "https://mymessagingstore.herokuapp.com/api/v1/sessions?key=#{key}&fbid=#{sender_id}&msg=#{msg}&first_name=#{username}&sender=bot&context=#{session.context}&profile_picture=#{profile_picture}"
+  url = "https://mymessagingstore.herokuapp.com/api/v1/sessions?bot_id=#{ENV['mms_token']}key=#{key}&fbid=#{sender_id}&msg=#{msg}&first_name=#{username}&sender=bot&context=#{session.context}&profile_picture=#{profile_picture}"
   RestClient.post URI.encode(url), :content_type => :json, :accept => :json
 end
 
