ee0d317f694c4e4581e4e76c12938168679e26b7
Matthias Richard <matthias.rms@gmail.com>
2016-06-28 14:41:17 +0200

    fix

--- app/bot/Structured messages/Countries/countries.rb
+++ app/bot/Structured messages/Countries/countries.rb
@@ -1,25 +1,26 @@
 # method for laying out the 3 geographical areas
 
 def country(session, sender, msg)
+  if session.count < 6
+    sender.reply({text: t('city-navigation-intro')})
+    transfer_middle_office(session.id, sender, t('city-navigation-intro'), "bot")
 
-  sender.reply({text: t('city-navigation-intro')})
-  transfer_middle_office(session.id, sender, t('city-navigation-intro'), "bot")
+    structured_reply = GenericTemplate.new
 
-  structured_reply = GenericTemplate.new
+    usa_button = Button.new
+    usa_button.add_postback("Check US cities","USA")
+    structured_reply.add_element("Top 10 US cities", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712864/SongKick/us_flag.png", "", [usa_button.get_message] )
 
-  usa_button = Button.new
-  usa_button.add_postback("Check US cities","USA")
-  structured_reply.add_element("Top 10 US cities", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712864/SongKick/us_flag.png", "", [usa_button.get_message] )
+    uk_button = Button.new
+    uk_button.add_postback("Check UK cities","UK")
+    structured_reply.add_element("Top 10 UK cities", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712861/SongKick/uk_flag.png", "", [uk_button.get_message] )
 
-  uk_button = Button.new
-  uk_button.add_postback("Check UK cities","UK")
-  structured_reply.add_element("Top 10 UK cities", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712861/SongKick/uk_flag.png", "", [uk_button.get_message] )
+    europe_button = Button.new
+    europe_button.add_postback("Check European cities","Europe")
+    structured_reply.add_element("Top 10 European cities", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712869/SongKick/europe_flag.png", "", [europe_button.get_message] )
 
-  europe_button = Button.new
-  europe_button.add_postback("Check European cities","Europe")
-  structured_reply.add_element("Top 10 European cities", "", "http://res.cloudinary.com/dpy7x8rgs/image/upload/v1464712869/SongKick/europe_flag.png", "", [europe_button.get_message] )
-
-  reply_transfer(session, sender, structured_reply)
+    reply_transfer(session, sender, structured_reply)
+  end
 end
 
 
